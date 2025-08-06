96b8b25716ac07d6ce274e16692f47ac65cb83d8
Md Asif Rahman Alif <mdalifr2928@gmail.com>
2025-08-06 22:10:06 +0600

    initial commit

---
+++ portfolio/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import './Navbar.css'
+
+
+const Navbar = () => {
+    return (
+       <header className="header">
+        <a href="#" className="logo">TeamWorks</a>
+
+        <nav className="navbar">
+            <a href="/ ">Home</a>
+            <a href="/ ">Pages</a>
+            <a href="/ ">Blog</a>
+            <a href="/ ">Portfolio</a>
+            <a href="/ ">Shop</a>
+            <a href="/ ">Contacts</a>
+
+        </nav>
+
+       </header>
+    )
+}
+
+export default Navbar
+
